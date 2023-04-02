043a506e85c8c4b3b0f6fa53e13544f7929f3d15
Akhil Gupta <akhilgupta@akhils-air.lan>
2023-04-02 16:38:12 -0700

    Updates

--- precache-manifest.898678059a86c9e0c8901140a9e95a34.js
+++ precache-manifest.898678059a86c9e0c8901140a9e95a34.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e2c3da990d91a9d487db32933b3398b7",
+    "revision": "5bdd63786a28d1ca2acceb3392bf517e",
     "url": "/index.html"
   },
   {
-    "revision": "62b4f1e08ac7c393cd85",
+    "revision": "88660380e14a05a2dc84",
     "url": "/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "376270b45e116dd6d859",
+    "revision": "ba9fbab6850bdc847de2",
     "url": "/static/css/main.db6f0745.chunk.css"
   },
   {
-    "revision": "62b4f1e08ac7c393cd85",
-    "url": "/static/js/2.af761574.chunk.js"
+    "revision": "88660380e14a05a2dc84",
+    "url": "/static/js/2.a1ef2b31.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/static/js/2.af761574.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a1ef2b31.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "376270b45e116dd6d859",
-    "url": "/static/js/main.a671d87c.chunk.js"
+    "revision": "ba9fbab6850bdc847de2",
+    "url": "/static/js/main.b03acfea.chunk.js"
   },
   {
     "revision": "b40ac64892122ac55d40",
