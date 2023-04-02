fbf12a24e2506690000032841a34ffc386cb6a81
Akhil Gupta <akhilgupta@akhils-air.lan>
2023-04-02 15:55:16 -0700

    changing website

---
+++ build/precache-manifest.718052c90f97ab2bdd5cec6156064d6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eaef077ab5477ae0aee403510508064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b7a513d75f493ec5adf",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "203266d49a243d2c09de",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "7b7a513d75f493ec5adf",
+    "url": "/static/js/2.cce256bc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.cce256bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203266d49a243d2c09de",
+    "url": "/static/js/main.dd3c239f.chunk.js"
+  },
+  {
+    "revision": "b40ac64892122ac55d40",
+    "url": "/static/js/runtime-main.07421a42.js"
+  }
+]);
